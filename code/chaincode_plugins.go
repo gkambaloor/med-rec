54e5cf26a087f239e81e1e347840496aa0706d97
Guruprasad Kambaloor <Guruprasad Kambaloor>
2017-02-20 23:17:28 +0530

    Initial code

--- code/chaincode_plugins.go
+++ code/chaincode_plugins.go
@@ -19,6 +19,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"encoding/json"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -27,6 +28,26 @@ import (
 type SimpleChaincode struct {
 }
 
+type VisitData struct {
+	HospitalId	string
+	Doctor		string
+	DoctorId	string
+	Diagnosis	string
+	Notes		string
+	Medication	string
+}
+type PatientData struct {
+	FirstName	string
+	MiddleName	string
+	LastName	string
+	Sex		string
+	DOB		string
+	Weight		string
+	Height		string
+	PatientId	string
+	Visits		[] VisitData
+	
+}
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
@@ -40,7 +61,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	err := stub.PutState("hello_world", []byte(args[0]))
+	err := stub.PutState("123456789", []byte(args[0]))
 	if err != nil {
 		return nil, err
 	}
@@ -48,7 +69,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	return nil, nil
 }
 
-// Invoke isur entry point to invoke a chaincode function
+// Invoke is the entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
@@ -57,7 +78,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
 		return t.write(stub, args)
+	} else if function == "create" {
+		return t.create(stub, args)
 	}
+
 	fmt.Println("invoke did not find func: " + function)
 
 	return nil, errors.New("Received unknown function invocation: " + function)
@@ -76,19 +100,73 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	return nil, errors.New("Received unknown function query: " + function)
 }
 
+// create - invoke function to create a new patient
+func (t *SimpleChaincode) create(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+	fmt.Println("running create()")
+
+	if len(args) != 8 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 8")
+	}
+	var patData PatientData
+	patData.FirstName = args[0]
+	patData.MiddleName = args[1]
+	patData.LastName = args[2]
+	patData.Sex = args[3]
+	patData.DOB = args[4]
+	patData.Weight = args[5]
+	patData.Height = args[6]
+	patData.PatientId = args[7]
+	//s:= fmt.Sprintf("%v", patData)
+	b,err := json.Marshal(patData)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	err = stub.PutState(args[7], b) 
+	//write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+
 // write - invoke function to write key/value pair
 func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, value string
+	var key,  jsonResp string
 	var err error
 	fmt.Println("running write()")
 
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	if len(args) != 7 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 7. ")
 	}
 
-	key = args[0] //rename for funsies
-	value = args[1]
-	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	key = args[0] //Patient Id
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	var patData PatientData
+	err = json.Unmarshal(valAsbytes, &patData)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Error unmarshalling for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	var visitData VisitData
+	visitData.HospitalId = args[1]
+	visitData.Doctor = args[2]
+	visitData.DoctorId = args[3]
+	visitData.Diagnosis = args[4]
+	visitData.Notes = args[5]
+	visitData.Medication = args[6]
+	patData.Visits = append(patData.Visits,visitData)
+	b,err1 := json.Marshal(patData)
+	if err1 != nil {
+		fmt.Println("error:", err1)
+	}
+
+	err = stub.PutState(key, b) //write the variable into the chaincode state
 	if err != nil {
 		return nil, err
 	}
@@ -99,6 +177,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
+	
 
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
@@ -110,6 +189,11 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-
+	var patData PatientData
+	err = json.Unmarshal(valAsbytes, &patData)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Error unmarshalling for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
 	return valAsbytes, nil
 }
